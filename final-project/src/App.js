d05f9ddad61654f41ae6a35e0e7c6ac95c6e7594
marc <marc_eiron_hernandez@dlsl.edu.ph>
2024-11-25 09:46:01 +0800

    added new files

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LoginForm from './components/LoginForm/Loginform';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is app.js</h1>
+    <div>
+      <LoginForm />
     </div>
   );
 }
---
+++ final-project/src/components/LoginForm/Loginform.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './LoginForm.css';
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FcGoogle } from "react-icons/fc";
+
+
+
+
+const LoginForm = () => {
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>USER LOGIN</h1>
+                <div className='input-box'>
+                    <input type='text' placeholder='Email' required />
+                    <MdEmail className='icon' />
+                </div>
+                <div className='input-box'>
+                    <input type='Password' placeholder='Password' required />
+                    <RiLockPasswordFill className='icon' />
+                </div>
+
+                <div className='forgot'>
+                    <a href='#/'>Forgot Password?</a>
+                </div>
+
+                <button type='submit' className='login'>Log-in to account</button>
+                <div className='google' >
+                <button type='button' href='#gmail.com'>
+                <FcGoogle className='googleicon' />
+                Continue with Google
+                </button>
+                </div>
+
+                <div className='register-link'>
+                    <p>Don't have an account? </p>
+                    <button type='button' className='create-acc'>Create New</button>
+                </div>
+
+                <div className='host-login'>
+                    <p>Want to host events? <a href='#/'>Click here</a></p>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
