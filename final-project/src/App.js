26bd0ce30276021a533b254e4edfad97172f16cc
jiro <jiro_rafael_layug@dlsl.edu.ph>
2024-11-25 09:55:31 +0800

    added the title cards, login shortcut and about page in main menu

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,8 +1,12 @@
 import { Navbar } from './components/Navbar';
+import {TitleCard} from './components/TitleCard';
+import {AccShortcut} from './components/AccShortcut';
+import About from './components/About';
 import './App.css';
 import homepage from './assets/homepage.png';
 
 function App() {
+
   return (
       <div className="App">
         <header className="App-header">
@@ -10,9 +14,19 @@ function App() {
         </header>
         <div className="container1">
           <img src={homepage} alt='' className='background1'/>
-          <div className="source-button">
+            <div className="source-button">
             <button className='button-59'>Services Provided</button>
-          </div>
+            </div>
+        </div>
+        <div className="container2">
+          <TitleCard />
+          <AccShortcut />
+        </div>
+        <div className="container3">
+          <About />
+        </div>
+        <div className="container4">
+          
         </div>
       </div>
   );
---
+++ final-project/src/components/TitleCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../css/TitleCard.css';
+
+export const TitleCard = () => {
+  return (
+    <div className="title-card-container">
+        <div className="title-card">
+            <h1>Title Card 1</h1>
+        </div>
+        <div className="title-card">
+            <h1>Title Card 2</h1>
+        </div>
+        <div className="title-card">
+            <h1>Title Card 3</h1>
+        </div>
+        <div className="title-card">
+            <h1>Title Card 4</h1>
+        </div>
+        <div className="title-card">
+            <h1>Title Card 5</h1>
+        </div>
+    </div>
+  );
+};
