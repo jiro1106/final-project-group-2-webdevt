38b5c02bfc999d1b336bb69e90c0dcce25288719
jiro <jiro_rafael_layug@dlsl.edu.ph>
2024-11-26 08:39:23 +0800

    created the contact, footer and fixed the responsiveness of AccShortcut

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,6 +2,8 @@ import { Navbar } from './components/Navbar';
 import {TitleCard} from './components/TitleCard';
 import {AccShortcut} from './components/AccShortcut';
 import About from './components/About';
+import {Contact} from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 import homepage from './assets/homepage.png';
 
@@ -26,7 +28,10 @@ function App() {
           <About />
         </div>
         <div className="container4">
-          
+          <Contact />
+        </div>
+        <div className="container5">
+          <Footer />
         </div>
       </div>
   );
