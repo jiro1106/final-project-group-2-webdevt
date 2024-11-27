03a4977a02e5e9b6e212c1104f8bc5945ec586cd
rem <relli_emmanuel_javier@dlsl.edu.ph>
2024-11-27 20:36:14 +0800

    modified the menupage components

--- final-project/src/components/Thumbnail.js
+++ final-project/src/components/Thumbnail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../App.css';
 import homepage from '../assets/homepage.png';
+import { Link } from "react-router-dom";
 
 export const Thumbnail = () => {
 
@@ -8,7 +9,7 @@ export const Thumbnail = () => {
         <div className="thumbnail">
         <img src={homepage} alt='' className='background1'/>
             <div className="source-button">
-             <button className='button-59'>Services Provided</button>
+             <Link to='/services'><button className='button-59'>Services Provided</button></Link>
             </div>
         </div>
     );
