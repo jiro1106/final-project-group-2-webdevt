726f6a413354eceb2e04fb5f3017a85b664ee4bd
jiro <jiro_rafael_layug@dlsl.edu.ph>
2024-12-02 23:24:17 +0800

    added design for event host create form

--- final-project/src/components/CreateForm.js
+++ final-project/src/components/CreateForm.js
@@ -3,7 +3,7 @@ import '../css/CreateForm.css';
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { FaUser  } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 const CreateForm = () => {
     const navigate = useNavigate();
@@ -83,8 +83,8 @@ const CreateForm = () => {
         <div className='form-page'>
             <div className='wrapper-create'>
                 <form onSubmit={handleSubmit}>
-                    <h1>CREATE NEW EVENT-HOST ACCOUNT</h1>
-                    
+                    <h1>CREATE NEW EVENT HOST ACCOUNT</h1>
+                    <h2 className="become-user-h2">Want to find events?<Link to="/CreateFormUser"><a className="host-shortcut-login"> Click here</a></Link></h2> 
                     <div className='input-box-create'>
                         <input type='text' name='firstName' placeholder='First Name' onChange={handleChange} required />
                         <FaUser  className='icon-create' />
@@ -114,7 +114,19 @@ const CreateForm = () => {
                         <label className='icon-create'>Upload ID Picture:</label>
                         <input type='file' accept='image/*' onChange={(e) => setIdPicture(e.target.files[0])} required />
                     </div>
-
+                    <div className='checkbox-18above-create-user'>
+                        <label>
+                        <input type='checkbox' required />
+                        18 years old and Above
+                        </label>
+                    </div>
+                    <div className='checkbox-terms-create-user'>
+                        <label>
+                        <input type='checkbox' required />
+                        Agree to terms and conditions
+                        </label>
+                    </div>
+                    <p className="login-shortcut">Already have an account?<Link to="/event-host"><a className="link-login-shortcut"> Log In here</a></Link></p>
                     <button type='submit' className='create-account-form'>Create Account</button>
                 </form>
             </div>
